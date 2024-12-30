a796eb227fa031c26f4bad258f7f60dff2d7f1b5
anibalalpizar <anibal.alpizar14@gmail.com>
2024-12-30 01:51:03 -0600

    ✨ Update project descriptions and add new projects to enhance project showcase

--- src/utils/IconMap.ts
+++ src/utils/IconMap.ts
@@ -8,6 +8,9 @@ import {
   XmlIcon,
   LandingIcon,
   NPMIcon,
+  NodeJSIcon,
+  MongoDBIcon,
+  ReactIcon
 } from "../icons"
 
 export const ICON_MAP = {
@@ -20,6 +23,9 @@ export const ICON_MAP = {
   XML: XmlIcon,
   Landing: LandingIcon,
   NPM: NPMIcon,
+  NodeJS: NodeJSIcon,
+  MongoDB: MongoDBIcon,
+  React: ReactIcon
 } as const
 
 export type IconName = keyof typeof ICON_MAP
